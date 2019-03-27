acee050a0293308fe6922e35b6a93aae15e4595e
zhangzhang <>
2019-03-27 12:23:59 +0800

    IP

--- pomelo-master/web-server/public/js/config/config.js
+++ pomelo-master/web-server/public/js/config/config.js
@@ -3,7 +3,7 @@ __resources__["/config.js"] = {
   data: function(exports, require, module, __filename, __dirname) {
     module.exports = {
       IMAGE_URL: 'http://pomelo.netease.com/art/',
-      GATE_HOST: window.location.hostname,
+      GATE_HOST: "192.168.1.107",//window.location.hostname,
       GATE_PORT: 3014
     };
   }
