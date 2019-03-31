95768ec52ede4ce62e4feafc527da90d1857bac3
zhangzhang <>
2019-03-31 14:45:23 +0800

    connector完成测试

--- HelloWorld/web-server/app.js
+++ HelloWorld/web-server/app.js
@@ -10,43 +10,29 @@ var options = {
 
 var app = express();
 
-// app.use(express.static('public'));
-// app.configure(function () {
-//   app.use(express.methodOverride());
-//   app.use(express.bodyParser());
-//   app.use(app.router);
+app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(app.router);
+  app.set('view engine', 'jade');
+  app.set('views', __dirname + '/public');
+  app.set('view options', {layout: false});
+  app.set('basepath',__dirname + '/public');
+});
 
-// });
-
-app.set('view engine', 'jade');
-app.set('views', __dirname + '/public');
-app.set('view options', { layout: false });
-app.set('basepath', __dirname + '/public');
-
-// app.configure('development', function () {
-//   app.use(express.static(__dirname + '/public'));
-//   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-// });
-let env = process.env.NODE_ENV || 'development';
-
-let errorhandler = express.errorHandler;
-
-if('development' == env){
+app.configure('development', function(){
   app.use(express.static(__dirname + '/public'));
-  app.use(errorhandler({log:false}));
-}
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
-
-if('production' == env) {
+app.configure('production', function(){
   var oneYear = 31557600000;
   app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-  app.use(errorhandler());
-};
-
-
-
-//var httpsServer = https.createServer(options, app);
-
-app.listen(3001, () => {
-  console.log("Web server has started.");
+  app.use(express.errorHandler());
 });
+
+console.log("Web server has started.\nPlease log on http://127.0.0.1:3001/index.html");
+
+var httpsServer = https.createServer(options, app);
+
+httpsServer.listen(3001);
